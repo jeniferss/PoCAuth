e2d996ddcf0d4f6b352149de47963e47660b708f
jenifers-s <67614296+jeniferss@users.noreply.github.com>
2022-08-12 18:54:02 -0300

    add vault: Instance and Connector

---
+++ auth-api/settings.py
@@ -0,0 +1,9 @@
+from vault_connector import get_credentials
+
+CREDENTIALS = get_credentials(path='credentials')
+
+KEYCLOAK_HOST = CREDENTIALS['keycloakHost']
+KEYCLOAK_REALM = CREDENTIALS['keycloakRealm']
+
+KONG_SERVICE_ID = CREDENTIALS['kongServiceId']
+KONG_SERVICE_SECRET = CREDENTIALS['kongServiceSecret']
---
+++ serviceB/settings.py
@@ -0,0 +1,8 @@
+from vault_connector import get_credentials
+
+CREDENTIALS = get_credentials(path='credentials')
+
+HOST = CREDENTIALS['kongHost']
+
+SERVICE_ID = CREDENTIALS['servicebId']
+SERVICE_SECRET = CREDENTIALS['servicebSecret']
